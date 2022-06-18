689d9025ada08ee11706099bee367ac8d5723952
KrTeaparty <hjk125ha@naver.com>
2022-06-18 18:01:57 +0900

    web html finished

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICIFILES_DIRS=[os.path.join(BASE_DIR, 'polls/static'),]
